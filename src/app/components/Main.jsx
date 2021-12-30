106b56ef22dfdc7600b6074d57d8b9ff01ad3227
Bryan O'Connor <oconnorjunkmail@gmail.com>
2021-12-30 10:51:46 -0700

    adds sagas, task links and task details to routing for interactive app

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -5,14 +5,20 @@ import {ConnectedDashboard} from "./Dashboard";
 import {Router, Route} from "react-router-dom";
 import {history} from "../store/history";
 import {ConnectedNavigation} from "./Navigation";
+import {ConnectTaskDetail} from "./TaskDetail";
 
 export const Main = () => (
 	<Router history={history}>
 		<Provider store={store}>
-			<div>
+			<>
 				<ConnectedNavigation />
 				<Route exact path="/dashboard" render={() => <ConnectedDashboard />} />
-			</div>
+				<Route
+					exact
+					path="/task/:id"
+					render={({match}) => <ConnectTaskDetail match={match} />}
+				/>
+			</>
 		</Provider>
 	</Router>
 );
